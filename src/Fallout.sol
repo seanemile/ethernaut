a9f66c253824353b133f37ac7893f771b30187ad
Sean Emile <seanamani.sa@gmail.com>
2023-02-04 23:52:43 +0300

    Added natspec comments

--- src/Fallout.sol
+++ src/Fallout.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.6.0;
 
 import "./lib/SafeMath.sol";
+/// @title A title that should describe the contract/interface
+/// @author The name of the author
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
 
 contract Fallout {
     using SafeMath for uint256;
